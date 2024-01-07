bc07550a7f19f8f25987ac2d922c79807255c556
navid-shakibayi <naweed.sh1474@gmail.com>
2024-01-08 02:31:00 +0330

    initial commit

---
+++ scripts.js
@@ -0,0 +1,90 @@
+$(document).on("click", "#hotDrinkIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#hotDrink").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#browingIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#browing").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#teeIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#tee").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#coldDrinkIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#coldDrink").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#icedCoffeeIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#icedCoffee").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#shakeIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#shake").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#cakeIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#cake").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#friesIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#fries").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#pastaIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#pasta").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#burgureIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#burgure").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#paniniIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#panini").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#pizzaIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#pizza").offset().top -160
+    }, 1000);
+});
+
+$(document).on("click", "#breakfastIcon", function () {
+
+    $("html, body").animate({
+        scrollTop: $("#breakfast").offset().top -160
+    }, 1000);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'rad-1': '#000',
+            'rad-2': '#fff',
+            'rad-3': '#c3c3c3',
+            'rad-4': '#f7f7f7',
+            'rad-5': '#e48e66',
+            'rad-6': '#1a1c1e',
+            'rad-7': '#333537',
+        },
+
+        fontFamily: {
+            'IRANSans': ['IRANSans', "sanserif"],
+            'IRANSans-thin': ['IRANSans-thin', "sanserif"],
+            'IRANSans-extralight': ['IRANSans-extralight', "sanserif"],
+            'IRANSans-light': ['IRANSans-light', "sanserif"],
+            'IRANSans-medium': ['IRANSans-medium', "sanserif"],
+            'IRANSans-semibold': ['IRANSans-semibold', "sanserif"],
+            'IRANSans-bold': ['IRANSans-bold', "sanserif"],
+            'IRANSans-extrabold': ['IRANSans-extrabold', "sanserif"],
+            'IRANSans-black': ['IRANSans-black', "sanserif"],
+        },
+
+        extend: {},
+    },
+    plugins: [],
+}
+
